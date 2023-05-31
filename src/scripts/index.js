5b560d458c48e9526727a5dba99d46c949ddf4cd
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-05-31 10:14:39 -0500

    load images from github and add responsiveness

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,21 @@
 'use-strict';
 
 import {select, print, onEvent } from './utils.js';
-import cityInfo from './cities.js';
+import cityInfo from './cities.json' assert { type: "json" };;
+import movieInfo from './movie-titles.json' assert { type: "json" };
 
-const cityList = select('section .main-container .filter-container .search-container input[type="text"]');
-const cityUrl = "./src/scripts/cities.json";
+const citySearchInput = select('section .main-container .filter-container .search-container input[type="text"]');
+const citiesUrl = "./src/scripts/cities.json";
 
-const movieTitleList = select('section .main-container .filter-container .title-search');
-const movieTitleUrl = "./src/scripts/movie-titles.json";
+const movieSearchInput = select('section .main-container .filter-container .title-search');
+const moviesUrl = "./src/scripts/movie-titles.json";
 
 const movieList = select('.movie-grid');
-const autocompleteResults = select('.autocomplete-results');
-const movieImgUrl = 'https://github.com/mrspecht/media/blob/main/img/avengers-infinity-war.jpg';
-const movies = [];
+const movieAutocompleteResults = select('.autocomplete-results');
 
+const movieTitles = [];
+const movieImages = [];
+const cities = [];
 
 // setup options
 const options = {
@@ -25,52 +27,61 @@ const options = {
 };
 
 // populate movie poster grid
-function listMovies(imgArray) {
-    movieList.innerHTML = "";
-    let movies = "";
-
-    if(imgArray.length > 0) {
-        for(let i = 0; i < imgArray.length; i++) {
-            movies += `
-            <div class="movie-card">
-                <img class="movie-img" src="${imgArray[i]}" alt="">
-                <p class="title"></p>
-            </div>
+function printMovies() {
+    fetch(moviesUrl)
+      .then(response => response.json())
+      .then(data => {
+        movieList.innerHTML = "";
+        let movieCard = "";
+  
+        if (data.movies.length > 0) {
+          data.movies.forEach(movie => {
+            movieCard += `
+              <div class="movie-card">
+                <img class="movie-img" src="${movie.image}" alt="${movie.title}">
+                <p class="title">${movie.title}</p>
+              </div>
             `;
+          });
+        } else {
+          movieCard += `<div>Movies not found</div>`;
         }
-    }
-    else {
-        movies += `<div>Movies not found</div>`;
-    }
-
-    movieList.innerHTML = `${movies}`;
-}
+  
+        movieList.innerHTML = movieCard;
+      })
+      .catch(error => {
+        console.error('Error fetching movie titles:', error);
+      });
+  }
 
 // reusable method to fetch from json source
-async function getArray(url) {
-    try {
-        const response = await fetch(url, options);
+// async function getArray(url) {
+//     try {
+//         const response = await fetch(url, options);
 
-        if(!response.ok) {
-            throw new Error(`${response.statusText} (${response.status})`);
-        }
+//         if(!response.ok) {
+//             throw new Error(`${response.statusText} (${response.status})`);
+//         }
 
-        const data = await response.json();
-        print(data);
-        return data;
-    }
-    catch(error) {
-        print(error.Message);
-        return []
-    }
-}
+//         const data = await response.json();
+//         console.log(data);
+//         return data;
+//     }
+//     catch(error) {
+//         print(error.Message);
+//         return []
+//     }
+// }
+
+// // fetch movie posters and populate grid
+printMovies();
 
 // Fetch the movie titles from the JSON file and populate an array to be used for autocomplete
-function populateAutoCompleteMovieSearch() {
-    return fetch(movieTitleUrl)
+function populateAutoCompleteMovieSearchList() {
+    return fetch(moviesUrl)
         .then(response => response.json())
         .then(data => {
-            movies.push(...data.titles.map(movie => movie.title));
+            movieTitles.push(...data.movies.map(movie => movie.title));
         })
         .catch(error => {
             console.error('Error fetching movie titles:', error);
@@ -80,7 +91,7 @@ function populateAutoCompleteMovieSearch() {
 // Function to filter and display autocomplete suggestions
 function showAutocompleteSuggestions(input) {
     const inputValue = input.value.toLowerCase();    
-    const autocompleteSuggestions = movies.filter(title => title.toLowerCase().includes(inputValue));
+    const autocompleteSuggestions = movieTitles.filter(title => title.toLowerCase().includes(inputValue));
     
     // Clear previous suggestions
     clearAutocompleteSuggestions();
@@ -97,21 +108,21 @@ function showAutocompleteSuggestions(input) {
             clearAutocompleteSuggestions();
         });
         
-        autocompleteResults.appendChild(suggestionItem);
+        movieAutocompleteResults.appendChild(suggestionItem);
     });
 }
   
 // Function to clear autocomplete suggestions
 function clearAutocompleteSuggestions() {
-    while (autocompleteResults.firstChild) {
-        autocompleteResults.removeChild(autocompleteResults.firstChild);
+    while (movieAutocompleteResults.firstChild) {
+        movieAutocompleteResults.removeChild(movieAutocompleteResults.firstChild);
     }
 }
 
-// Bind the 'keyup' event to the input field
-populateAutoCompleteMovieSearch().then(() => {
-    onEvent('keyup', movieTitleList, () => {
-        const input = movieTitleList;
+// Load the autocomplete search lists and then invoke the keyup bindings
+populateAutoCompleteMovieSearchList().then(() => {
+    onEvent('keyup', movieSearchInput, () => {
+        const input = movieSearchInput;
         showAutocompleteSuggestions(input);
 
         // Check if input is empty and call clearAutocompleteSuggestions
@@ -123,7 +134,7 @@ populateAutoCompleteMovieSearch().then(() => {
 
 // if any area outside the movie title search box is clicked, close the suggestions
 document.addEventListener('click', function(event) {
-    if (event.target !== movieTitleList) {
+    if (event.target !== movieSearchInput) {
         clearAutocompleteSuggestions();
     }
 });
--- src/scripts/movie-titles.js
+++ src/scripts/movie-titles.js
@@ -1,94 +1,124 @@
 export default {  
-    "titles": [
+    "movies": [
         {
-        "title": "Avengers Infinity War"
+        "title": "Avengers Infinity War",
+        "image": "https://github.com/mrspecht/media/main/img/avengers-infinity-war.jpg"
         },
         {
-        "title": "Back to the Future"
+        "title": "Back to the Future",
+        "image": "https://github.com/mrspecht/media/main/img/back-to-the-future.jpg"
         },
         {
-        "title": "Eternals"
+        "title": "Eternals",
+        "image": "https://github.com/mrspecht/media/main/img/eternals.jpg"
         },
         {
-        "title": "F9 The Fast Saga"
+        "title": "F9 The Fast Saga",
+        "image": "https://github.com/mrspecht/media/main/img/f9-the-fast-saga.jpg"
         },
         {
-        "title": "Gone in 60 Seconds"
+        "title": "Gone in 60 Seconds",
+        "image": "https://github.com/mrspecht/media/main/img/gone-in-60-seconds.jpg"
         },
         {
-        "title": "JFK"
+        "title": "JFK",
+        "image": "https://github.com/mrspecht/media/main/img/jfk.jpg"
         },
         {
-        "title": "Jurassic Park"
+        "title": "Jurassic Park",
+        "image": "https://github.com/mrspecht/media/main/img/jurassic-park.jpg"
         },
         {
-        "title": "My Cousin Vinny"
+        "title": "My Cousin Vinny",
+        "image": "https://github.com/mrspecht/media/main/img/my-cousin-vinny.jpg"
         },
         {
-        "title": "No Time to Die"
+        "title": "No Time to Die",
+        "image": "https://github.com/mrspecht/media/main/img/no-time-to-die.jpg"
         },
         {
-        "title": "Oceans Eleven"
+        "title": "Oceans Eleven",
+        "image": "https://github.com/mrspecht/media/main/img/oceans-eleven.jpg"
         },
         {
-        "title": "Revenge of the Sith"
+        "title": "Revenge of the Sith",
+        "image": "https://github.com/mrspecht/media/main/img/revenge-of-the-sith.jpg"
         },
         {
-        "title": "Robocop"
+        "title": "Robocop",
+        "image": "https://github.com/mrspecht/media/main/img/robocop.jpg"
         },
         {
-        "title": "Rocky"
+        "title": "Rocky",
+        "image": "https://github.com/mrspecht/media/main/img/rocky.jpg"
         },
         {
-        "title": "Spiderman No Way Home"
+        "title": "Spiderman No Way Home",
+        "image": "https://github.com/mrspecht/media/main/img/spider-man-no-way-home.jpg"
         },
         {
-        "title": "Terminator 2 Judgement Day"
+        "title": "Terminator 2 Judgement Day",
+        "image": "https://github.com/mrspecht/media/main/img/terminator-2-judgment-day.jpg"
         },
         {
-        "title": "The Batman"
+        "title": "The Batman",
+        "image": "https://github.com/mrspecht/media/main/img/the-batman.jpg"
         },
         {
-        "title": "The Devil Wears Prada"
+        "title": "The Devil Wears Prada",
+        "image": "https://github.com/mrspecht/media/main/img/the-devil-wears-prada.jpg"
         },
         {
-        "title": "The Exorcist"
+        "title": "The Exorcist",
+        "image": "https://github.com/mrspecht/media/main/img/the-exorcist.jpg"
         },
         {
-        "title": "The Fellowship of the Ring"
+        "title": "The Fellowship of the Ring",
+        "image": "https://github.com/mrspecht/media/main/img/the-fellowship-of-the-ring.jpg"
         },
         {
-        "title": "The Flash"
+        "title": "The Flash",
+        "image": "https://github.com/mrspecht/media/main/img/the-flash.jpg"
         },
         {
-        "title": "The Godfather"
+        "title": "The Godfather",
+        "image": "https://github.com/mrspecht/media/main/img/the-godfather.jpg"
         },
         {
-        "title": "The Grand Budapest Hotel"
+        "title": "The Grand Budapest Hotel",
+        "image": "https://github.com/mrspecht/media/main/img/the-grand-budapest-hotel.jpg"
         },
         {
-        "title": "The Lion King"
+        "title": "The Lion King",
+        "image": "https://github.com/mrspecht/media/main/img/the-lion-king.jpg"
         },
         {
-        "title": "The Matrix"
+        "title": "The Matrix",
+        "image": "https://github.com/mrspecht/media/main/img/the-lion-king.jpg"
         },
         {
-        "title": "The Others"
+        "title": "The Others",
+        "image": "https://github.com/mrspecht/media/main/img/the-others.jpg"
         },
         {
-        "title": "The Remains of the Day"
+        "title": "The Remains of the Day",
+        "image": "https://github.com/mrspecht/media/main/img/the-remains-of-the-day.jpg"
         },
         {
-        "title": "The Shining"
+        "title": "The Shining",
+        "image": "https://github.com/mrspecht/media/main/img/the-shining.jpg"
         },
         {
-        "title": "Titanic"
+        "title": "Titanic",
+        "image": "https://github.com/mrspecht/media/main/img/titanic.jpg"
         },
         {
-        "title": "Toy Story 3"
+        "title": "Toy Story 3",
+        "image": "https://github.com/mrspecht/media/main/img/toy-story-3.jpg"
         },
         {
-        "title": "West Side Story"
+        "title": "West Side Story",
+        "image": "https://github.com/mrspecht/media/main/img/west-side-story.jpg"
         }
     ]
 }
